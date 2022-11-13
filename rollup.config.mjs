0cdbb73f0d5483fb0e87b50f92e4f31d6c4d842c
mh_sattarian <mh_sattarian@outlook.com>
2022-11-13 12:22:06 +0330

    change node version in GH actions

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,16 +3,14 @@ import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
-export default [
-  {
-    input: "src/index.js",
-    plugins: [resolve(), commonjs(), terser()],
-    output: {
-      file: pkg.main,
-      format: "umd",
-      exports: "named",
-      name: "SplitPolygonMode",
-      sourcemap: process.env.NODE_ENV !== "production",
-    },
+export default {
+  input: "src/index.js",
+  plugins: [resolve(), commonjs(), terser()],
+  output: {
+    file: pkg.main,
+    format: "umd",
+    exports: "named",
+    name: "SplitPolygonMode",
+    sourcemap: process.env.NODE_ENV !== "production",
   },
-];
+};
