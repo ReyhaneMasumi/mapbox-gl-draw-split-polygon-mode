372f78e2cca716d7d8dc9fc27c5afb8656381a97
mh_sattarian <mh_sattarian@outlook.com>
2022-11-16 14:14:51 +0330

    WIP: Reviewing performance

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,13 @@
 export const modeName = "split_polygon";
+
+/// This mode uses the `mapbox-gl-draw-passing-mode` mode to draw the spilitting lineString.
+/// here is the name used to add that mode:
 export const passingModeName = `${modeName}__passing_draw_line_string`;
-export const highlightPropertyName = "splitPolygon_highlight";
+
+/// when a (multi-)polygon feature is selected to be splitted, it gets highlighted.
+/// here is the name of the property indicating the highlight.
+export const highlightPropertyName = `${modeName}_highlight`;
+
 export const defaultOptions = {
   highlightColor: "#222",
   lineWidth: 0.001,
